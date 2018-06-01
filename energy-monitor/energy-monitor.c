051a6379c7612184caf5f8bc93a27e53dec03171
David Greasley <davidgreasley@btopenworld.com>
2018-06-01 20:27:55 +0100

    Added basic energy measurement code

--- energy-monitor/energy-monitor.c
+++ energy-monitor/energy-monitor.c
@@ -12,6 +12,9 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_t launch_thread;
 pthread_t measure_thread;
 
+// Sample rate in Hz
+const float sample_rate = 100;
+
 char command[100];
 
 void* start_benchmark(void *param)
@@ -31,10 +34,37 @@ void* start_benchmark(void *param)
 
 void* measure_energy(void *param)
 {
+    long long energy = 0;
+    long long previous_value = -1;
+    long long current_value = -1;
+
     while(!benchmark_complete)
     {
-        printf("Measuring energy\n");
-        sleep(1);
+        printf("Measuring energy.");
+
+        FILE *fff = fopen("/sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj", "r");
+        if (fff==NULL)
+        {
+            printf("Error: Cannot access RAPL counters\n");
+        }
+        else
+        {
+            fscanf(fff, "%lld", &current_value);
+            fclose(fff);
+        }
+
+        if (previous_value > -1) 
+        {
+            if (current_value < previous_value)
+            {
+                // handle overflow
+            }
+            energy += current_value - previous_value;
+        }
+
+        previous_value = current_value;
+
+        sleep(1/sample_rate);
     }
 }
 
