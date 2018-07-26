323bd8770ecc8a139177b20b6fb841f22b33b31a
David Greasley <davidgreasley@btopenworld.com>
2018-07-26 15:08:44 +0100

    Reverting some spec benchmarks back to train size

--- benchmarks/spec_omp2012/botsalgn/validate.sh
+++ benchmarks/spec_omp2012/botsalgn/validate.sh
@@ -3,4 +3,5 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
 
-specperl $SPEC/bin/specdiff -m -l 10  --abstol 1e-07 $SPEC/benchspec/OMP2012/358.botsalgn/data/ref/output/botsalgn.out botsalgn.out > botsalgn.out.cmp
+specperl $SPEC/bin/specdiff -m -l 10  --abstol 1e-07 $SPEC/benchspec/OMP2012/358.botsalgn/data/train/output/botsalgn.out botsalgn.out > botsalgn.out.cmp
+
