3cc7f4a7e276f973e2e597a5d75584866b8f82ec
David Greasley <davidgreasley@btopenworld.com>
2018-08-08 17:18:35 +0100

    Updated run files for botsspar, imagick and nab benchmarks

--- benchmarks/spec_omp2012/nab/run.sh
+++ benchmarks/spec_omp2012/nab/run.sh
@@ -3,6 +3,8 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
 
-./nabmd aminos 391519156 > aminos.out 2>> aminos.err
-./nabmd gcn4 1850041461 > gcn4.out 2>> gcn4.err
-
+for i in {1..2}
+do
+    ./nabmd aminos 391519156 > aminos.out 2>> aminos.err
+    ./nabmd gcn4 1850041461 > gcn4.out 2>> gcn4.err
+done
