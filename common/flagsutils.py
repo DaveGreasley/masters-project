052e9b79f4aa527fc42479fd74ab73593763443a
David Greasley <davidgreasley@btopenworld.com>
2018-08-17 15:51:18 +0100

    Refactored CE and config generator scripts to use single flag list

---
+++ common/flagsutils.py
@@ -0,0 +1,8 @@
+import io
+
+from common.basedirectory import *
+
+
+def load_flag_list():
+    with io.open(base_dir + '/gcc_flags_all.csv') as flag_file:
+        return [f.rstrip() for f in flag_file]
