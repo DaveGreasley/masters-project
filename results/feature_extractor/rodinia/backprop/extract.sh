9e93582af898dae9464773d755bea8b0bf6ee2ad
David Greasley <davidgreasley@btopenworld.com>
2018-09-01 20:25:09 +0000

    Added features for some rodinia benchmarks

---
+++ results/feature_extractor/rodinia/backprop/extract.sh
@@ -0,0 +1,11 @@
+ck set env tags=compiler,ctuning-cc bat_file=tmp-ck-env.sh --bat_new --print && . ./tmp-ck-env.sh
+
+whereis $CK_CC
+whereis $CK_F95
+whereis $CK_CXX
+
+
+#$CK_CC -O3 -c -DSPEC -DSPEC_OMP -DSPEC_OPENMP -DNDEBUG -I. -fopenmp  magick_effect.c --ct-extract-features -lm
+
+
+$CK_CC -g -fopenmp -O3 /home/masters-project/benchmarks/rodinia/backprop/backprop.c -c --ct-extract-features
