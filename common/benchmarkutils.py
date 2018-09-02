9a5b2cad925ea86b90141279cf7f9107724c9d3c
David Greasley <davidgreasley@btopenworld.com>
2018-09-02 23:13:18 +0100

    Updated benchmark utils to use basedirectory properly

--- common/benchmarkutils.py
+++ common/benchmarkutils.py
@@ -1,4 +1,5 @@
-from common.basedirectory import *
+import common.basedirectory as basedirectory
+
 from common.model import NPB
 from common.model import SPEC
 from common.model import Parboil
@@ -6,39 +7,39 @@ from common.model import Rodinia
 
 def get_available_benchmarks():
     return [
-        NPB('BT', 'C', npb_dir),
-        #NPB('BT', 'C', npb_dir, version='VEC'),
-        NPB('CG', 'C', npb_dir),
-        NPB('EP', 'D', npb_dir),
-        NPB('FT', 'C', npb_dir),
-        NPB('IS', 'D', npb_dir),
-        NPB('LU', 'C', npb_dir),
-        #NPB('LU', 'C', npb_dir, version='VEC'),
-        NPB('MG', 'D', npb_dir),
-        NPB('SP', 'C', npb_dir),
-        NPB('UA', 'C', npb_dir),
-        SPEC('botsalgn', spec_dir),
-        SPEC('botsspar', spec_dir),
-        SPEC('bwaves', spec_dir),
-        SPEC('fma3d', spec_dir),
-        SPEC('ilbdc', spec_dir),
-        SPEC('kdtree', spec_dir),
-        SPEC('md', spec_dir),
-        SPEC('nab', spec_dir),
-        SPEC('imagick', spec_dir),
-        SPEC('smithwa', spec_dir),
-        SPEC('swim', spec_dir),
-        Parboil('tpacf', 'small', parboil_dir),
-        Parboil('stencil', 'default', parboil_dir),
-        Parboil('lbm', 'long', parboil_dir),
-        Parboil('bfs', '1M', parboil_dir),
-        Parboil('histo', 'medium', parboil_dir),
-        Parboil('mri-gridding', 'smaller', parboil_dir),
-        Rodinia('backprop', rodinia_dir),
-        Rodinia('hotspot', rodinia_dir),
-        Rodinia('kmeans', rodinia_dir),
-        Rodinia('srad', rodinia_dir),
-        Rodinia('streamcluster', rodinia_dir)
+        NPB('BT', 'C', basedirectory.npb_dir),
+        #NPB('BT', 'C', basedirectory.npb_dir, version='VEC'),
+        NPB('CG', 'C', basedirectory.npb_dir),
+        NPB('EP', 'D', basedirectory.npb_dir),
+        NPB('FT', 'C', basedirectory.npb_dir),
+        NPB('IS', 'D', basedirectory.npb_dir),
+        NPB('LU', 'C', basedirectory.npb_dir),
+        #NPB('LU', 'C', basedirectory.npb_dir, version='VEC'),
+        NPB('MG', 'D', basedirectory.npb_dir),
+        NPB('SP', 'C', basedirectory.npb_dir),
+        NPB('UA', 'C', basedirectory.npb_dir),
+        SPEC('botsalgn', basedirectory.spec_dir),
+        SPEC('botsspar', basedirectory.spec_dir),
+        SPEC('bwaves', basedirectory.spec_dir),
+        SPEC('fma3d', basedirectory.spec_dir),
+        SPEC('ilbdc', basedirectory.spec_dir),
+        SPEC('kdtree', basedirectory.spec_dir),
+        SPEC('md', basedirectory.spec_dir),
+        SPEC('nab', basedirectory.spec_dir),
+        SPEC('imagick', basedirectory.spec_dir),
+        SPEC('smithwa', basedirectory.spec_dir),
+        SPEC('swim', basedirectory.spec_dir),
+        Parboil('tpacf', 'small', basedirectory.parboil_dir),
+        Parboil('stencil', 'default', basedirectory.parboil_dir),
+        Parboil('lbm', 'long', basedirectory.parboil_dir),
+        Parboil('bfs', '1M', basedirectory.parboil_dir),
+        Parboil('histo', 'medium', basedirectory.parboil_dir),
+        Parboil('mri-gridding', 'smaller', basedirectory.parboil_dir),
+        Rodinia('backprop', basedirectory.rodinia_dir),
+        Rodinia('hotspot', basedirectory.rodinia_dir),
+        Rodinia('kmeans', basedirectory.rodinia_dir),
+        Rodinia('srad', basedirectory.rodinia_dir),
+        Rodinia('streamcluster', basedirectory.rodinia_dir)
     ]
 
 
